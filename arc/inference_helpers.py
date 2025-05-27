fix label masking

arc/inference_helpers.py
@@ -18,6 +18,7 @@ class ARCInferencer:
         tokenizer: PreTrainedTokenizer,
         generation_config: GenerationConfig,
         parse_grid_fn: Callable[[List[int]], Grid],
+        fmt_opts: dict,
     ):
         self.model = model
         self.tokenizer = tokenizer
@@ -25,6 +26,7 @@ class ARCInferencer:
         self.model.generation_config = self.generation_config
         self.parse_grid_fn = parse_grid_fn
         self.device = model.device
+        self.fmt_opts = fmt_opts
 
     def parse_grid(self, ids: List[int]) -> Grid:
         return self.parse_grid_fn(ids)
@@ -62,29 +64,40 @@ class ARCInferencer:
     def _generate(
         self, 
         batch_datapoints: List[DataPointDict], 
-        return_logits: bool = False
+        return_logits: bool = False,
+        use_static_shape: bool = False,
     ) -> List[List[int]] | List[tuple[List[int], torch.Tensor]]:
-        prompt_messages = [arc_utils.format_prompt_messages(dp) for dp in batch_datapoints]
-        prompt_strs = [
-            self.tokenizer.apply_chat_template(
-                prompt_msg,
-                tokenize=False,
-                add_generation_prompt=True,
-                continue_final_message=False,
-                enable_thinking=False,
-            )
-            for prompt_msg in prompt_messages
-        ]
+        input_start = self.fmt_opts.get("input_start", "")
+        input_end = self.fmt_opts.get("input_end", "")
+        output_end = self.fmt_opts.get("output_end", "")
+        preprompt = self.fmt_opts.get("preprompt", "")
+        prompt_messages = [arc_utils.format_prompt_messages(dp, self.tokenizer, input_start, input_end, output_end, preprompt) for dp in batch_datapoints]
+        # prompt_strs = [
+        #     self.tokenizer.apply_chat_template(
+        #         prompt_msg,
+        #         tokenize=False,
+        #         add_generation_prompt=True,
+        #         continue_final_message=False,
+        #         enable_thinking=False,
+        #     )
+        #     for prompt_msg in prompt_messages
+        # ]
         
+        # model_inputs = self.tokenizer(
+        #     text=prompt_strs,
+        #     add_special_tokens=False,
+        #     return_tensors="pt",
+        #     padding=True,
+        #     truncation=True, 
+        # ).to(self.device)
+
         model_inputs = self.tokenizer(
-            text=prompt_strs,
+            text=prompt_messages,
             add_special_tokens=False,
             return_tensors="pt",
-            padding=True,
-            truncation=True, 
         ).to(self.device)
 
-        prompt_lens = model_inputs["input_ids"].ne(self.tokenizer.pad_token_id).long().sum(dim=1)
+        prompt_lens = [len(ids) for ids in model_inputs["input_ids"]]
 
         with torch.no_grad():
             output = self.model.generate(
@@ -92,6 +105,16 @@ class ARCInferencer:
                 return_dict_in_generate=return_logits,
                 output_scores=return_logits,
             )
+        
+        seq = [output[i][prompt_lens[i]:].cpu().tolist() for i in range(len(output))]
+
+        if use_static_shape:
+            size_list = [self._infer_test_shape(dp) for dp in batch_datapoints]
+            for i in range(len(seq)):
+                x,y=size_list[i]
+                seq[i]=seq[i][:x*(y+1)]
+            return seq
+        
         
         if not return_logits:
             # output: (batch_size, total_seq_len)
@@ -260,7 +283,7 @@ class ARCInferencer:
         self,
         base_datapoint: DataPointDict,
     ) -> Grid:
-        output_ids = self._generate([base_datapoint], return_logits=False)[0]
+        output_ids = self._generate([base_datapoint], return_logits=False, use_static_shape=False)[0]
         try:
             parsed_grid = self.parse_grid(output_ids)
             grid_aug = np.array(parsed_grid)